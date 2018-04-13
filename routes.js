c0624a3c198646744bcba719941d9afe7fcd472a
Erico Netto <eng.erico@gmail.com>
2018-04-12 22:11:23 -0300

    adding CSV endpoint

--- routes.js
+++ routes.js
@@ -27,6 +27,43 @@ function getHospitals(req, res, next) {
 }
 
 
+
+function getHospitalsCsv(req, res, next) {
+  var location=[req.body.lat, req.body.lng];
+  var pagetoken=req.query.pagetoken;
+
+
+  hospitals.hospitalsSP(location, pagetoken).then(result => {
+
+
+
+      const Json2csvParser = require('json2csv').Parser;
+      const fields = ['name', 'address_components', 'adr_address', 'formatted_address', 'loaction', 'icon', 'vicinity', 'website', 'formatted_phone_number', 'opening_hours', 'photos', 'reviews', 'rating'];
+      const opts = { fields };
+       
+      try {
+        const parser = new Json2csvParser(opts);
+        const csv = parser.parse(result.results);
+        res.setHeader('Content-disposition', 'attachment; filename=hospitais.csv');
+        res.set('Content-Type', 'text/csv');
+        res.status(200).send(csv);
+      } catch (err) {
+        console.error(err);
+      }
+
+
+
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+ 
+  
+}
+
+
+
 module.exports = {
-  getHospitals: getHospitals
+  getHospitals: getHospitals,
+  getHospitalsCsv: getHospitalsCsv
 };
--- routes/index.js
+++ routes/index.js
@@ -47,4 +47,32 @@ var routes = require('../routes');
 router.post('/api/Hospitals', routes.getHospitals);
 
 
+/**
+ * @swagger
+ * /api/HospitalsCSV:
+*   post:
+*     tags:
+*       - Hospitals
+*     description: Get all hospitals from SP
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: location
+*         description: Current user location
+*         in: body
+*         required: true
+*         schema:
+*           $ref: '#/definitions/Location'
+*       - name: pagetoken
+*         description: (optional) Each call retrieves a maxium 20 hospitals, and then a token for the next 20
+*         in: query
+*         schema:
+*           type: string
+*     responses:
+*       200:
+*         description: Ok
+*/
+router.post('/api/HospitalsCSV', routes.getHospitalsCsv);
+
+
 module.exports = router;
